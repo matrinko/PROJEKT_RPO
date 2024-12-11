46e7726b1bb05f33f82cb7325aca521b39d8f9fe
NikKrnjovsek <nik.krnjovsek@gmail.com>
2024-12-11 07:44:47 +0100

    bugfixes

--- planitbackend/routes/users.js
+++ planitbackend/routes/users.js
@@ -34,7 +34,7 @@ router.post('/login', async (req, res) => {
 router.post('/getuser', async (req, res) => {
   try {
     const { username } = req.body;
-    let user = await User.find({username : username });
+    const user = await User.find({username : username });
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json({ message: 'Napaka pri pridobivanju userja.', error: err });
