7b05773bc7a2a8280bf63bd6e50ca7f95ef7b66b
matrinko <martin.kobal@student.um.si>
2024-12-11 19:18:59 +0100

    okzadnje

--- planitbackend/routes/projects.js
+++ planitbackend/routes/projects.js
@@ -25,7 +25,7 @@ router.get('/:username', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {  //req.body
+router.get('/dobi/:id', async (req, res) => {  //req.body
   try {
     const { id } = req.params
     console.log(id)
