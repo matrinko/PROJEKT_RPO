aa47b71634ca7debdba50d315501f524c0c7a97a
NikKrnjovsek <nik.krnjovsek@gmail.com>
2025-01-09 02:09:39 +0100

    dodal kodo za profilno sliko

--- planitbackend/models/User.js
+++ planitbackend/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema(
         email: { type: String, required: true },
         prijatelji: {type: Array, default : []},
         password: {type: String, required: true},
+        image: {type: String, required: false},
     },
     { timestamps: true } 
 );
--- planitbackend/routes/users.js
+++ planitbackend/routes/users.js
@@ -19,6 +19,8 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     const {username, password} = req.body;
+    console.log(username);
+    console.log(password);
     const users = await User.find({ username: username, password: password });
     if (users && users.length > 0) {
       res.status(200).json({ success: true, username: users[0].username, _id: users[0]._id });
@@ -38,6 +40,39 @@ router.post('/getuser', async (req, res) => {
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json({ message: 'Napaka pri pridobivanju userja.', error: err });
+  }
+});
+
+router.post('/setprofileimage', async (req, res) => {
+  try {
+    const { username, image } = req.body;
+    const user = await User.findOneAndUpdate(
+      { username: username },
+      { $set: { image: image } },
+      { new: true }
+    );
+    if (user) {
+      res.status(200).json({ success: true, message: 'Profilna slika posodobljena.', user });
+    } else {
+      res.status(404).json({ success: false, message: 'Uporabnik ni najden.' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Napaka pri posodabljanju profilne slike.', error: err });
+  }
+});
+
+// Pridobi profilno sliko uporabnika
+router.post('/getprofileimage', async (req, res) => {
+  try {
+    const { username } = req.body;
+    const user = await User.findOne({ username: username });
+    if (user && user.image) {
+      res.status(200).json({ success: true, image: user.image });
+    } else {
+      res.status(404).json({ success: false, message: 'Profilna slika ni najdena.' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Napaka pri pridobivanju profilne slike.', error: err });
   }
 });
 
